0ed609df6b0157905bded90d1339973437c08cab
Rene Ramos <neo_ramos@yahoo.com>
2012-05-27 17:31:08 -0400

    added mixed case email test to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   #attr_accessor :password, :password_confirmation
   has_secure_password
   
-  before_save { |user| user.email = email.downcase }
+  before_save { self.email.downcase! }
+  # an alternative ====>>>> before_save { |user| user.email = email.downcase }
     
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -98,6 +98,17 @@ describe User do
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+	
+  end
+  
+  describe "Email address with mix case" do
+    let(:mixed_case_email) {"Foo@ExAMPle.CoM"}
+	
+	it "should be saved in lower-case" do
+	  @user.email = mixed_case_email
+	  @user.save
+	  @user.reload.email.should == mixed_case_email.downcase
+	end
   end
   
 end
